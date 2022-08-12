bf58e8421238a48a53b67527e1ea69507237a6d7
Naoya Nitta <n-nitta@konan-u.ac.jp>
2022-08-12 17:09:36 +0900

    Initial commit.

---
+++ src/main/java/resources/Shipping.java
@@ -0,0 +1,17 @@
+package resources;
+import values.Item;
+import values.ItemHandling;
+
+import java.util.*;
+
+public class Shipping {
+	Handling handling;
+
+	public Shipping(Handling handling) {
+		this.handling = handling;
+	}
+
+	public Item getValue() {
+		return new Item(handling.getValue().getName(), handling.getValue().getShippingNum());
+	}
+}
